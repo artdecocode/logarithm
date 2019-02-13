299c45752ef7e34ec516a3ddbfbf2e80207a52cd
Anton <anton@adc.sh>
2019-02-14 01:25:43 +0300

    b

--- build/index.js
+++ build/index.js
@@ -1,30 +1,94 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('logarithm')
+let uniqid = require('uniqid'); if (uniqid && uniqid.__esModule) uniqid = uniqid.default;
+const { aqt } = require('rqt');
+const { req } = require('./lib');
 
 /**
- * The Logs.Page Middleware For Web-Server To Record Hits.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * Create a middleware for logging requests.
+ * @param {Config} options Options for the program.
+ * @param {string} options.app The name of the website application.
+ * @param {string} options.url ElasticSearch endpoint URL, e.g., `http://192.168.0.1:9200`.
+ * @param {number} [options.timeout=5000] Timeout for the connection after which an error is shown. Default `5000`.
+ * @param {string} [options.type="hit"] The type of the document. Default `hit`.
+ * @param {string} [options.pipeline="info"] The pipeline in ElasticSearch, for example to parse GeoIP info and User-Agent. Default `info`.
+ * @param {string} [options.index] The name of the index. Defaults to the app name if not specified as well as monthly strategy.
+ * @param {('monthly')} [options.strategy="monthly"] How to construct the index name. E.g., the monthly strategy will result in `${index}-${y}.${m}` indexes. Default `monthly`.
  */
-               async function logarithm(config = {}) {
+const logarithm = (options) => {
+  if (!options) throw new Error('Options are not given')
   const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('logarithm called with %s', text)
-  return text
+    app, index = app, pipeline = 'info', url, type = 'hit',
+  } = options
+  if (!app) throw new Error('The app is not defined')
+  /** @type {import('koa').Middleware} */
+  const es = async (ctx, next) => {
+    let e; try { await next() } catch (err) { e = err }
+    const {
+      request: { ip, path },
+      headers: {
+        cookie, // eslint-disable-line no-unused-vars
+        ...headers
+      },
+      status,
+    } = ctx
+    const date = new Date()
+    const body = {
+      app,
+      ip,
+      path,
+      headers,
+      status,
+      date,
+    }
+
+    const id = uniqid()
+    const i = getIndex(index, date)
+    const u = `${url}/${i}/${type}/${id}/_create`
+    req(u, {
+      spec: {
+        method: 'POST',
+        timeout: 5000,
+      },
+      query: { pipeline },
+    }, body).then(() => {
+      // process.stdout.write('.')
+    }).catch(({ message }) => {
+      console.log(`Logarithm ERROR: ${message}`)
+    })
+
+    if (e) throw e
+  }
+  return es
 }
+
+/**
+ * Check that a connection to the server can be established.
+ * @param {string} url The ElasticSearch URL.
+ * @param {number} [timeout=30000] The timeout for the request in ms. Default `30000`.
+ */
+       const ping = async (url, timeout = 30000) => {
+  const { statusCode } = await aqt(url, { timeout, justHeaders: true, method: 'HEAD' })
+  if (statusCode != 200) throw new Error(`Server responded with status code ${statusCode}`)
+}
+
+const getIndex = (index, date) => {
+  const y = date.getFullYear()
+  const m = date.getMonth() + 1
+  return `${index}-${y}.${m}`
+}
+
+module.exports=logarithm
 
 /* documentary types/index.xml */
 /**
  * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * @prop {string} app The name of the website application.
+ * @prop {string} url ElasticSearch endpoint URL, e.g., `http://192.168.0.1:9200`.
+ * @prop {number} [timeout=5000] Timeout for the connection after which an error is shown. Default `5000`.
+ * @prop {string} [type="hit"] The type of the document. Default `hit`.
+ * @prop {string} [pipeline="info"] The pipeline in ElasticSearch, for example to parse GeoIP info and User-Agent. Default `info`.
+ * @prop {string} [index] The name of the index. Defaults to the app name if not specified as well as monthly strategy.
+ * @prop {('monthly')} [strategy="monthly"] How to construct the index name. E.g., the monthly strategy will result in `${index}-${y}.${m}` indexes. Default `monthly`.
  */
 
 
-module.exports = logarithm
-//# sourceMappingURL=index.js.map
+module.exports.ping = ping
---
+++ build/lib/index.js
@@ -0,0 +1,96 @@
+let rqt = require('rqt'); if (rqt && rqt.__esModule) rqt = rqt.default;
+const { stringify } = require('querystring');
+
+/**
+ * Sets up the info pipeline for parsing of user-agent and extracting GEOIP info from IP address.
+ * @param {string} url The ElasticSearch URL.
+ */
+       const setupPipeline = async (url, id) => {
+  const u = `${url}/_ingest/pipeline/${id}`
+  await req(u, {
+    spec: {
+      method: 'PUT',
+      timeout: 5000,
+    },
+  }, {
+    'description': 'IP Address And UserAgent',
+    'processors': [
+      { 'geoip': { 'field': 'ip' } },
+      { 'user_agent': { 'field': 'headers.user-agent' } },
+    ],
+  })
+}
+
+       const listPipelines = async (url) => {
+  const u = `${url}/_ingest/pipeline`
+  const res = await req(u, {
+    spec: {
+      timeout: 5000,
+    },
+  })
+  return res
+}
+       const listTemplates = async (url) => {
+  const u = `${url}/_template`
+  const res = await req(u, {
+    spec: {
+      timeout: 5000,
+    },
+  })
+  return res
+}
+       const stats = async (url) => {
+  const u = `${url}/_stats`
+  const res = await req(u, {
+    spec: {
+      timeout: 10000,
+    },
+  })
+  return res
+}
+
+       const deletePipeline = async (url, id) => {
+  const u = `${url}/_ingest/pipeline/${id}`
+  const res = await req(u, {
+    spec: {
+      method: 'DELETE',
+      timeout: 5000,
+    },
+  })
+  return res
+}
+
+/**
+ * Make the request.
+ * @param {string} url The URL.
+ * @param {{ spec: Spec, query: Object.<string, string>}} spec The specification.
+ */
+       const req = async (url, { spec, query = {} } = {}, body) => {
+  const q = stringify(query)
+  const p = /^https?:\/\//.test(url) ? url : `http://${url}`
+  const u = `${p}${q ? `?${q}` : ''}`
+  const res = await rqt(u, {
+    ...spec,
+    data: body,
+  }).then(({ 'error': error, ...rest }) => {
+    if (error) {
+      const e = typeof error == 'string' ? error : error['reason']
+      throw new Error(e)
+    }
+    return rest
+  })
+  return res
+}
+
+/**
+ * @typedef {Object} Spec
+ * @prop {string} method
+ * @prop {number} timeout
+ */
+
+module.exports.setupPipeline = setupPipeline
+module.exports.listPipelines = listPipelines
+module.exports.listTemplates = listTemplates
+module.exports.stats = stats
+module.exports.deletePipeline = deletePipeline
+module.exports.req = req
